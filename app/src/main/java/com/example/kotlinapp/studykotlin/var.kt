5f530d0c8418cd05dcc3df78fe07fccb098d084d
clh <lihu.chen@tusen.ai>
2025-03-19 15:58:44 +0800

    val

---
+++ app/src/main/java/com/example/kotlinapp/studykotlin/var.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinapp.studykotlin
+
+fun main() {
+    println("study kotlin")
+    testvar()
+//    testval()
+}
+
+fun testvar() {
+    var a: Int = 10
+    a *= 10
+    println(a)
+}
+
+fun testval() {
+    // val 定义的变量是只读的，所以这里会报错
+    val a: Int = 10
+    a *= 10
+    println(a)
+}
