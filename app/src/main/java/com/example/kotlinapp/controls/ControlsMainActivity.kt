3eebaaaa70d51ba79185059d9d87ea760c5e1edc
clh <lihu.chen@tusen.ai>
2025-03-21 14:53:24 +0800

    progress bar

---
+++ app/src/main/java/com/example/kotlinapp/controls/ControlsMainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinapp.controls
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinapp.R
+
+class ControlsMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_controls_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val progress_bar1 = findViewById<ProgressBar>(R.id.progress_bar1)
+        button1.setOnClickListener {
+            progress_bar1.progress = progress_bar1.progress + 5
+        }
+    }
+}
