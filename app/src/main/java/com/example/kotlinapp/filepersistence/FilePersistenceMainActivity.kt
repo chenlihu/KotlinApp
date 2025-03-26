2978a1481b30edf3d48135988b40e0c125888732
clh <lihu.chen@tusen.ai>
2025-03-26 11:15:33 +0800

    存储到文件

---
+++ app/src/main/java/com/example/kotlinapp/filepersistence/FilePersistenceMainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.kotlinapp.filepersistence
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinapp.R
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class FilePersistenceMainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_file_persistence_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        editText = findViewById<EditText>(R.id.editText)
+        val inputText = load()
+        if (inputText.isNotEmpty()) {
+            editText.setText(inputText)
+            editText.setSelection(inputText.length)
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        val inputText = editText.text.toString()
+        save(inputText)
+    }
+
+    fun load(): String {
+        var content = StringBuilder()
+        try {
+            val input = openFileInput("data")
+            var reader = BufferedReader(InputStreamReader(input))
+            reader.use {
+                // 内置扩展函数
+                it.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return content.toString()
+    }
+
+    private fun save(inputText: String) {
+        try {
+            // Context.MODE_PRIVATE 覆盖模式
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            // use 内置扩展函数, writer会自动关闭
+            writer.use {
+                it.write(inputText)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+}
